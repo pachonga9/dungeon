8f76cf1a445df6542ed2c5ab25525fd1337c3c55
Cody Wyers <cody@wyers.dev>
2021-09-24 21:43:10 -0500

    clean up configs

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log('meow');
+import promptSync from "prompt-sync";
+
+console.log('welcome to the dungeon');
+const name = promptSync()('What is your name? ');
+console.log(`Hey there ${name}`);
+
