565e901515c098fd320403207bdf5f0f643ea4af
pachonga9 <pachonga9@gmail.com>
2021-10-19 14:06:48 -0500

    merging functional game

---
+++ DungeonInReadline.ts
@@ -0,0 +1,165 @@
+import {stdin, stdout} from 'process';
+import * as readline from 'readline';
+
+const rl = readline.createInterface({input: stdin, output: stdout});
+
+const user = {
+    location: 0,
+    choices: ['1. Kick down the door', '2. Fight Monsters', '3. Run Away', '4. Exit Game'],
+    monsterBlock: false,
+    gold: 0,
+    farthestRoom: 0,
+};
+
+const spawnMonster = () => {
+    // user.monsterBlock = !user.monsterBlock;
+    user.monsterBlock = true;
+    console.log('A MONSTER SPAWNED IN FRONT OF YOU AND BLOCKS YOUR PATH!');
+    // console.log('A monster just spawned.');
+    // console.log(`DevComment: the monsterBlock is set to ${user.monsterBlock}`);
+};
+
+const getRandomInt = () => {
+    return Math.floor(Math.random() * 10);
+};
+
+const collectGold = () => {
+    let x = getRandomInt();
+    user.gold = user.gold + x;
+    if(x === 0){
+        console.log(`you search the monster's filfthy corpse, but find nothing of value.`);
+    } else {
+        console.log(`You search the monster's filfthy corpse and find ${x} gold.`);
+    }
+};
+
+const killMonster = () => {
+    // user.monsterBlock = !user.monsterBlock;
+    user.monsterBlock = false;
+    // console.log(`DevComment: the monsterBlock is set to ${user.monsterBlock}`);
+    console.log('YOUR SWORD CONNECTS! The monster lets out an agonized scream and crumples to the ground, dead. The way forward is clear.');
+    collectGold();
+    console.log(`You have ${user.gold} gold.`);
+};
+
+function fightMonster() {
+    if(user.monsterBlock){ // THIS IS TRUE
+        console.clear();
+        console.log(`You swing your sword at the monster.`);
+        killMonster();
+        getInput();
+    } else {
+        console.clear();
+        console.log(`There is no monster here to fight. You wrestle with your inner demons.`);
+        getInput();
+    }
+};
+
+function kickDoor() {
+    if(user.monsterBlock){ // THIS IS TRUE
+        console.clear();
+        console.log(`You can't move forward while the monster blocks your path.`);
+        getInput();
+    } else if(user.location >= user.farthestRoom){ // if the current location greater than or equal to the farthest room...
+        console.clear();
+        console.log('You kick down the sturdy door in front of you. YOU MOVE UP ONE ROOM!');
+        user.location++;
+        logFarthestRoom();
+        console.log(`the farthest room you have travelled is to room ${user.farthestRoom}.`);
+        // spawnMonster();
+        getInput();
+    } else if(user.location < user.farthestRoom ){ // if the current location is less than the to farthest room...
+        console.clear();
+        console.log('You have already kicked down this door. You move deeper into the dungeon.');
+        console.log(`A rotting corpse of a monster lay dead on the floor.`);
+        user.location++;
+        logFarthestRoom();
+        console.log(`The farthest room you have travelled is to room ${user.farthestRoom}.`);
+        getInput();
+    }
+};
+
+function runAway() {
+    if (user.location < 5){
+    console.clear();
+        rl.question('Are you sure you want to flee? (y/n) ', function (answer) {
+            if (answer === 'y') {
+                console.log(`Your father was right about you. You aren't cut out for adventure.`);
+                user.location = 0;
+            }
+            console.clear();
+            getInput();
+        });
+    } else if (user.location >= 5){
+        console.clear();
+        rl.question('Are you sure you would like to return to camp to rest? (y/n) ', function (answer) {
+            if (answer === 'y') {
+                console.log('You head back to camp.');
+                user.location = 0;
+            }
+            console.clear();
+            getInput();
+        });
+    }
+};
+
+function handleAnswer(answer: string) {
+    switch (answer) {
+        case '1':
+            kickDoor();
+            break;
+        case '2':
+            fightMonster();
+            break;
+        case '3':
+            runAway();
+            break;
+        case '4':
+            console.log(`Okay, goodbye.`)
+            process.exit();
+        default:
+            getInput();
+    }
+};
+
+function isLocationFive() {
+    if (user.location >= 5) {
+        user.choices.splice(2, 1, '3. Return to Camp');
+    } else if (user.location < 5) {
+        user.choices.splice(2, 1, '3. Run Away');
+        /// This seems kinda weird if I don't add the Run Away option again...
+    }
+};
+
+function getLocation() {
+    isLocationFive();
+    if (user.location === 0) { 
+        return `You are outside the dungeon. You have ${user.gold} gold. The farthest you have gone is room ${user.farthestRoom}.`;
+    }
+    return `You are in dungeon room #${user.location}`;
+};
+
+function logFarthestRoom() {
+    console.log('You check your map...');
+    if(user.location > user.farthestRoom){ // if the current location is greater than the furthest you have been...
+        console.log(`You update your map with the new area.`);
+        user.farthestRoom++;
+        spawnMonster();
+    }   else if(user.location <= user.farthestRoom){ // if the current location is less far as or equal to the furthest room...
+            console.log(`Your are in dungeon room ${user.location}. You have been here before.`);
+    }
+};
+
+const getInput = () => {
+    const location = getLocation();
+    console.log(location);
+    user.choices.forEach((item) => console.log(item));
+    rl.question('What would you like to do? ', function (answer) {
+        console.log(`you answered ${answer}`);
+        handleAnswer(answer);
+    });
+};
+
+console.clear();
+console.log('Welcome to Dungeon Crawler\n');
+getInput();
--- app.ts
+++ app.ts
@@ -22,36 +22,39 @@ console.log('Welcome to DUNGEON CRAWLER. Created by David Wyers.');
     console.log('You are outside of the dungeon.');
     let choice = getDecision();
     if(choice == '1'){
-        console.clear();
-        console.log(`You kick in the door. A monster is here.`);
-        console.log(room1());
-        } 
-        else if (choice == '2'){
+            console.clear();
+            console.log(`You kick in the door. A monster is here.`);
+            console.log(room1());
+        } else if (choice == '2'){
             console.clear();
             console.log(`The only monster here right now is your inability to make a propper decision. Make a different choice.`);
             console.log(gameStart());
         } else if (choice == '3'){
             console.clear();
             setTimeout(function() {
-            console.log(`your father was right about you. You arent cut out for adventure.`); 
-            }, 1000);
-            setTimeout(function() {
-            console.log(`You head back to camp for the night and return in the morning.`); 
-            }, 3000);
-            setTimeout(function() {
-            console.log(gameStart());
-            }, 6000);
+                console.log(`Your Father was right about you. You aren't cut out for adventure.`);
+                setTimeout(function(){
+                    console.log(`You head back to camp for the night and return in the morning.`);
+                    setTimeout(function(){
+                        console.log(gameStart());
+                    }, 2000)
+                }, 2000)
+            }, 2000);
         } else if (choice == '4'){
             console.clear();
             console.log('Fine. Leave. See if I care.');
             //end loop
         };
 }
+let doorStanding = true;
 let monsterIsAlive = true;
 const room1 = () => {
     const killMonster = () => {
         monsterIsAlive = !monsterIsAlive;
     }
+    const kickDoor = () => {
+        doorStanding = !doorStanding;
+    };
     if(monsterIsAlive){
         console.log('You are in dungeon room 1. A monster blocks your path.');
     } else {
@@ -63,8 +66,13 @@ const room1 = () => {
             if(monsterIsAlive){
             console.log(`You can't move forward until you kill the monster.`);
             console.log(room1());
-            } else {
+            } else if(doorStanding) {
             console.log('You kick down the door.');
+            kickDoor();
+            console.log(room1());
+            } else {
+                console.log(`You kick at nothing... I knew a boy like you in the old village. Chased the ducks around. "Touched", they called him.`)
+                console.log(room1());
             }
         } 
         else if (choice == '2'){
@@ -99,4 +107,4 @@ const room1 = () => {
 gameStart();
 }
 }
-//mine
+//mine2
