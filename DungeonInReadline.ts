4fdfcd40d954c836e733d6df41a4b5af01288d3e
pachonga9 <pachonga9@gmail.com>
2021-10-27 14:55:57 -0500

    Making a Class

--- DungeonInReadline.ts
+++ DungeonInReadline.ts
@@ -1,9 +1,12 @@
+
 import {stdin, stdout} from 'process';
 import * as readline from 'readline';
+class DungeonInReadline {
 
-const rl = readline.createInterface({input: stdin, output: stdout});
+    constructor(){}
+rl = readline.createInterface({input: stdin, output: stdout});
 
-const user = {
+user = {
     location: 0,
     choices: ['1. Proceed', '2. Fight Monster', '3. Run Away', '4. Exit Game'],
     monsterBlock: false,
@@ -12,20 +15,20 @@ const user = {
     didUserRun: false,
 };
 
-function spawnMonster(): void {
-    if (user.location === user.farthestRoom) {
-        user.monsterBlock = true;
+spawnMonster(): void {
+    if (this.user.location === this.user.farthestRoom) {
+        this.user.monsterBlock = true;
     }
-    user.didUserRun = false; //resetting this value here is clever. you dont currently need or use the value but its cool.
+    this.user.didUserRun = false; //resetting this value here is clever. you dont currently need or use the value but its cool.
 };
 
-function getRandomInt(): number {
+getRandomInt(): number {
     return Math.floor(Math.random() * 10);
 };
 
-function collectGold(): void {
-    let x: number = getRandomInt();
-    user.gold = user.gold + x;
+collectGold(): void {
+    let x: number = this.getRandomInt();
+    this.user.gold = this.user.gold + x;
     if (x === 0) {
         console.log(`you search the monster's filthy corpse, but find nothing of value.`);
     } else {
@@ -33,125 +36,125 @@ function collectGold(): void {
     }
 };
 
-function killMonster(): void {
-    user.monsterBlock = false;
+killMonster(): void {
+    this.user.monsterBlock = false;
     console.log('YOUR SWORD CONNECTS! The monster lets out an agonized scream and crumples to the ground, dead. The way forward is clear.');
-    collectGold();
-    console.log(`You have ${user.gold} gold.`);
+    this.collectGold();
+    console.log(`You have ${this.user.gold} gold.`);
 };
 
-function fightMonster(): void {
-    const isRoomOccupied: boolean = getIsRoomOccupied();
+fightMonster(): void {
+    const isRoomOccupied: boolean = this.getIsRoomOccupied();
     if (isRoomOccupied) {
         console.clear();
         console.log(`You swing your sword at the monster.`);
-        killMonster();
-        getInput();
+        this.killMonster();
+        this.getInput();
     } else {
         console.clear();
         console.log(`There is no monster here to fight. You wrestle with your inner demons.`);
-        getInput();
+        this.getInput();
     }
 }
 
-function getIsPreviousRoom(): boolean {
-    return user.location !== user.farthestRoom;
+getIsPreviousRoom(): boolean {
+    return this.user.location !== this.user.farthestRoom;
 }
 
-function getIsRoomOccupied(): boolean {
-    const isInPreviousRoom: boolean = getIsPreviousRoom();
+getIsRoomOccupied(): boolean {
+    const isInPreviousRoom: boolean = this.getIsPreviousRoom();
     if (isInPreviousRoom) {
         return false;
     }
-    return user.monsterBlock;
+    return this.user.monsterBlock;
 }
 
-function kickDoor(): void {
-    const isMonsterInThisRoom: boolean = getIsRoomOccupied();
+kickDoor(): void {
+    const isMonsterInThisRoom: boolean = this.getIsRoomOccupied();
     if (isMonsterInThisRoom) {
         console.clear();
         console.log(`You can't move forward while the monster blocks your path.`);
-        getInput();
+        this.getInput();
         return;
     }
 
-    const isInPreviousRoom: boolean = getIsPreviousRoom();
+    const isInPreviousRoom: boolean = this.getIsPreviousRoom();
     if (isInPreviousRoom) {
-        user.location++;
+        this.user.location++;
         console.clear();
         console.log('You have already kicked down this door. You move deeper into the dungeon.');
-        console.log(`You check your map. You are in dungeon room ${user.location}. You have been here before.`);
-        getInput();
+        console.log(`You check your map. You are in dungeon room ${this.user.location}. You have been here before.`);
+        this.getInput();
         return;
     }
 
-    user.location++;
-    user.farthestRoom++;
+    this.user.location++;
+    this.user.farthestRoom++;
     console.clear();
     console.log('You kick down the sturdy door in front of you. YOU MOVE UP ONE ROOM!');
-    console.log(`You update your map with a diagram of dungeon room ${user.location}`);
-    spawnMonster();
-    getInput();
+    console.log(`You update your map with a diagram of dungeon room ${this.user.location}`);
+    this.spawnMonster();
+    this.getInput();
 }
 
-function runAway(): void {
-    if (user.location < 5) {
+runAway(): void {
+    if (this.user.location < 5) {
         console.clear();
         rl.question('Are you sure you want to flee? (y/n) ', function (answer: string) {
             if (answer === 'y') {
                 console.log(`Your father was right about you. You aren't cut out for adventure.`);
-                user.location = 0;
-                user.didUserRun = true;
+                this.user.location = 0;
+                this.user.didUserRun = true;
             }
             console.clear();
-            getInput();
+            this.getInput();
         });
-    } else if (user.location >= 5) {
+    } else if (this.user.location >= 5) {
         console.clear();
         rl.question('Are you sure you would like to return to camp to rest? (y/n) ', function (answer: string) {
             if (answer === 'y') {
                 console.log('You head back to camp.');
-                user.location = 0;
+                this.user.location = 0;
             }
             console.clear();
-            getInput();
+            this.getInput();
         });
     }
 }
 
-function handleAnswer(answer: string): void {
+handleAnswer(answer: string): void {
     switch (answer) {
         case '1':
-            kickDoor();
+            this.kickDoor();
             break;
         case '2':
-            fightMonster();
+            this.fightMonster();
             break;
         case '3':
-            runAway();
+            this.runAway();
             break;
         case '4':
             console.log(`Okay, goodbye.`)
             process.exit();
             return;
         default:
-            getInput();
+            this.getInput();
             return;
     }
 }
 
-function describeLocation(): void {
-    if (user.location === 0) {
-        const message: string = `You are outside the dungeon. You have ${user.gold} gold. The farthest you have gone is room ${user.farthestRoom}.`;
+describeLocation(): void {
+    if (this.user.location === 0) {
+        const message: string = `You are outside the dungeon. You have ${this.user.gold} gold. The farthest you have gone is room ${this.user.farthestRoom}.`;
         console.log(message);
         return;
     }
-    const roomOccupied: boolean = getIsRoomOccupied();
+    const roomOccupied: boolean = this.getIsRoomOccupied();
     if (roomOccupied) {
         console.log('A monster blocks your path');
         return;
     }
-    const isPreviousRoom: boolean = getIsPreviousRoom();
+    const isPreviousRoom: boolean = this.getIsPreviousRoom();
     if (isPreviousRoom) {
         console.log('A rotting corpse is on the ground.');
         return;
@@ -160,30 +163,32 @@ function describeLocation(): void {
     console.log('A fresh corpse is on the ground.');
 }
 
-function getInput(): void {
-    describeLocation();
-    if (user.location === 0) {
-        console.log(user.choices[0]);
-        console.log(user.choices[3]);
-    } else if (user.location < user.farthestRoom) {
-        console.log(user.choices[0]);
-        console.log(user.choices[2]);
-        console.log(user.choices[3]);
-    } else if (user.location === user.farthestRoom && user.monsterBlock) {
-        console.log(user.choices[1]);
-        console.log(user.choices[2]);
-        console.log(user.choices[3]);
-    } else if (user.location === user.farthestRoom && user.monsterBlock === false) {
-        console.log(user.choices[0]);
-        console.log(user.choices[2]);
-        console.log(user.choices[3]);
+getInput(): void {
+    this.describeLocation();
+    if (this.user.location === 0) {
+        console.log(this.user.choices[0]);
+        console.log(this.user.choices[3]);
+    } else if (this.user.location < this.user.farthestRoom) {
+        console.log(this.user.choices[0]);
+        console.log(this.user.choices[2]);
+        console.log(this.user.choices[3]);
+    } else if (this.user.location === this.user.farthestRoom && this.user.monsterBlock) {
+        console.log(this.user.choices[1]);
+        console.log(this.user.choices[2]);
+        console.log(this.user.choices[3]);
+    } else if (this.user.location === this.user.farthestRoom && this.user.monsterBlock === false) {
+        console.log(this.user.choices[0]);
+        console.log(this.user.choices[2]);
+        console.log(this.user.choices[3]);
     }
-    rl.question('What would you like to do? ', function (answer: string) {
+    rl.question('What would you like to do? ', function (answer: string){
         console.log(`You answered ${answer}`);
-        handleAnswer(answer);
+        this.handleAnswer(answer);
     });
 }
-
+start = () => {
 console.clear();
 console.log('Welcome to Dungeon Crawler\n');
-getInput();
+this.getInput();
+};
+};
