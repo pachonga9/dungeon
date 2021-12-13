55c37c560e78b073a73db70fd563a66aff50f518
pachonga9 <pachonga9@gmail.com>
2021-12-13 15:34:34 -0600

    fix
    fix

--- src/game-state-type.ts
+++
@@ -1,6 +0,0 @@
-export enum GameStateType {
-  crawler,
-  inventory,
-  menu,
-  welcome
-}
--- src/game-state.ts
+++
@@ -1,6 +0,0 @@
-import {GameStateType} from "./game-state-type";
-
-export interface GameState {
-  open: (fromState: GameStateType) => void;
-  transitionToState: (state: GameStateType) => void;
-}
