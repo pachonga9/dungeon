58e4002ec4e090fbda25ce6b5a5e618cc0741360
pachonga9 <pachonga9@gmail.com>
2021-12-20 14:01:09 -0600

    bla

--- src/state/game-state.ts
+++ src/state/game-state.ts
@@ -3,20 +3,12 @@ import { Location } from "../locations/location";
 
 interface GameState {
   locations: Location[];
-  // currentLocation: number;
-  // lastLocation: number;
-  // farthestRoom: number;
   inventory: Inventory;
   notDone: boolean;
-  // playerLifeTotal: number;
 }
 
 export class DungeonGameState implements GameState {
   locations = [];
-  // currentLocation = 0;
-  // lastLocation = 0;
-  // farthestRoom = 0;
   inventory = new Inventory();
   notDone = true;
-  // playerLifeTotal = 500;
 }
